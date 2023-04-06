d65f6e1d5c31935ce079208adc5d59704d6f95b8
Renan Araujo <renanaraujo852@hotmail.com>
2023-04-06 08:03:34 -0300

    feat: user model

--- src/main/java/com/api/tweterooapi/dto/UserDTO.java
+++ src/main/java/com/api/tweterooapi/dto/UserDTO.java
@@ -4,8 +4,8 @@ import org.hibernate.validator.constraints.URL;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record userDTO(
-        @NotBlank String username,
-        @NotBlank @URL String avatar) {
+public record UserDTO(
+                @NotBlank String username,
+                @NotBlank @URL String avatar) {
 
 }
---
+++ src/main/java/com/api/tweterooapi/model/User.java
@@ -0,0 +1,32 @@
+package com.api.tweterooapi.model;
+
+import com.api.tweterooapi.dto.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+
+public class User {
+    public User(UserDTO data) {
+        this.username = data.username();
+        this.avatar = data.avatar();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 35, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String avatar;
+}
