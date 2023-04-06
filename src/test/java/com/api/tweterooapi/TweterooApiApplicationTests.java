94c1a01eaed778ecd968803ba4d234f9e0223101
Renan Araujo <renanaraujo852@hotmail.com>
2023-04-06 07:17:09 -0300

    feat: initializing maven project

---
+++ src/test/java/com/api/tweterooapi/TweterooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tweterooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
